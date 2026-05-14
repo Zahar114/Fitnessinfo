9d1cea3aa20fc9b106565f5a153361e59f8bdf2d
Mashmanuk zakhar <zaharmasmanukl@example.com>
2026-05-14 23:16:02 +0300

    Lab5

--- gym/urls.py
+++ gym/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('workouts/', views.workouts),
-    path('nutrition/', views.nutrition),
+    path('', views.home, name='home'),
+    path('category/<int:category_id>/', views.category_exercises, name='category_exercises'),
+    path('nutrition/', views.nutrition, name='nutrition'),
 ]
--- gym/views.py
+++ gym/views.py
@@ -1,27 +1,55 @@
 from django.shortcuts import render
+from .models import MuscleGroup, Exercise, NutritionPlan
 
 
 def home(request):
+    muscle_groups = MuscleGroup.objects.all()
+    exercises = Exercise.objects.all()
+    nutrition_plans = NutritionPlan.objects.all()
+
     context = {
-        'title': 'Fitness Info'
+        'title': 'Fitness Info',
+        'muscle_groups': muscle_groups,
+        'exercises': exercises,
+        'nutrition_plans': nutrition_plans,
     }
 
     return render(request, 'gym/home.html', context)
 
 
-def workouts(request):
+def category_exercises(request, category_id):
+    muscle_groups = MuscleGroup.objects.all()
+    category = MuscleGroup.objects.get(id=category_id)
+    exercises = Exercise.objects.filter(muscle_group=category)
+
     context = {
-        'title': 'Тренування',
-        'description': 'Тут знаходяться тренування для всіх груп м’язів.'
+        'title': category.name,
+        'muscle_groups': muscle_groups,
+        'category': category,
+        'exercises': exercises,
     }
 
-    return render(request, 'gym/workouts.html', context)
+    return render(request, 'gym/category.html', context)
 
 
 def nutrition(request):
+    muscle_groups = MuscleGroup.objects.all()
+    nutrition_plans = NutritionPlan.objects.all()
+
     context = {
         'title': 'Харчування',
-        'description': 'Правильне спортивне харчування для спортсменів.'
+        'muscle_groups': muscle_groups,
+        'nutrition_plans': nutrition_plans,
     }
 
     return render(request, 'gym/nutrition.html', context)
+def workouts(request):
+    muscle_groups = MuscleGroup.objects.all()
+    exercises = Exercise.objects.all()
+    
+    context = {
+        'title': 'Тренування',
+        'muscle_groups': muscle_groups,
+        'exercises': exercises,
+    }
+    return render(request, 'gym/workouts.html', context)
